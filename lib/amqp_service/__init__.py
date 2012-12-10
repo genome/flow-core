090e0938f9cf2cd0de8b945d15210c59d68c5aa4
Mark Burnett <mburnett@genome.wustl.edu>
2012-12-09 20:02:58 -0600

    add NullHandler in root __init__.py

--- lib/amqp_service/__init__.py
+++ lib/amqp_service/__init__.py
@@ -1,3 +1,18 @@
+import logging
+import logging.handlers
+
+try:
+    nh = logging.handlers.NullHandler()
+except AttributeError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+    nh = NullHandler()
+
+logging.getLogger().addHandler(nh)
+
+
 from amqp_manager import AMQPManager
 from service import AMQPService
 
