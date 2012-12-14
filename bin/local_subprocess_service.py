84d19f2a91cae72085c2299e94bbb402a383d311
Mark Burnett <mburnett@genome.wustl.edu>
2012-12-14 12:59:28 -0600

    add local subprocess server script to bin

---
+++ bin/local_subprocess_service.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import logging
+import os
+
+from amqp_service import ConnectionManager, AMQPService, dispatcher, responder
+
+LOG_LEVEL = logging.DEBUG
+LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
+              '-35s %(lineno) -5d: %(message)s')
+LOG = logging.getLogger()
+
+if '__main__' == __name__:
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    console_handler.setLevel(LOG_LEVEL)
+    LOG.addHandler(console_handler)
+    LOG.setLevel(LOG_LEVEL)
+
+    amqp_url = os.getenv('AMQP_URL')
+    if not amqp_url:
+        amqp_url = 'amqp://guest:guest@localhost:5672/workflow'
+    connection_manager = ConnectionManager(amqp_url)
+
+    subprocess_dispatcher = dispatcher.SubprocessDispatcher()
+    submit_responder = responder.DispatchResponder(
+            subprocess_dispatcher,
+            queue='subprocess_submit_job_requests', exchange='subprocess',
+            succeeded_routing_key='submit.respond')
+
+    service = AMQPService(connection_manager, submit_responder)
+
+    try:
+        service.run()
+    except KeyboardInterrupt:
+        service.stop()
