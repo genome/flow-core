ee47edeed6c3dc80e876ca9d8e7e29063d0d477b
Mark Burnett <mburnett@genome.wustl.edu>
2012-12-20 09:17:15 -0600

    make sure we set nil environment values to empty strings

--- lib/amqp_service/dispatcher/util.py
+++ lib/amqp_service/dispatcher/util.py
@@ -7,6 +7,10 @@ def environment(environment_dicts):
     for env in environment_dicts:
         temporary_environment.update(env)
 
+    for k, v in temporary_environment.iteritems():
+        if v is None:
+            temporary_environment[k] = ''
+
     saved_environment = dict(os.environ)
     os.environ.clear()
     os.environ.update(temporary_environment)
