01a280214b614815e6dd2e41c79c9136b7b34d91
Mark Burnett <mburnett@genome.wustl.edu>
2012-12-21 20:11:20 -0600

    move amqp manager to new package

--- lib/amqp_manager/__init__.py
+++ lib/amqp_manager/__init__.py
@@ -10,13 +10,7 @@ except AttributeError:
 
     nh = NullHandler()
 
-logging.getLogger('amqp_service').addHandler(nh)
+logging.getLogger('amqp_manager').addHandler(nh)
 
 # NOTE pika does not do this itself for some reason
 logging.getLogger('pika').addHandler(nh)
-
-
-from service import AMQPService
-
-import dispatcher
-import responder
