2bddd7bd191d47cc46bb709d9563eedc7cba31cc
Mark Burnett <mburnett@genome.wustl.edu>
2012-12-22 11:34:57 -0600

    move publisher confirm code to channel manager

--- lib/amqp_manager/channel_manager.py
+++ lib/amqp_manager/channel_manager.py
@@ -1,25 +1,30 @@
 import logging
 
+from pika.spec import Basic
+
 LOG = logging.getLogger(__name__)
 
 class ChannelManager(object):
-    def __init__(self, prefetch_count=None,
-            publisher_confirms=True, delegates=[]):
-        self._channel = None
+    def __init__(self, prefetch_count=None, delegates=[]):
         self.prefetch_count = prefetch_count
-        self.publisher_confirms = publisher_confirms
         self.delegates = delegates
 
+        self._channel = None
 
     def on_connection_open(self, connection):
-        LOG.debug('Creating channel')
+        LOG.debug('Connection open, creating channel')
         connection.channel(self._on_channel_open)
 
     def on_connection_closed(self, method_frame):
-        LOG.debug('Channel closed, method_frame = %s', method_frame)
+        LOG.debug('Got on_connection closed in channel_manager %s: %s',
+                self._channel, method_frame)
 
 
     def _on_channel_open(self, channel):
+        self._setup_channel(channel)
+        self._inform_delegates_about_channel(channel)
+
+    def _setup_channel(self, channel):
         self._channel = channel
 
         LOG.debug('Channel %s created, adding callback', channel)
@@ -30,16 +35,14 @@ class ChannelManager(object):
                     channel, self.prefetch_count)
             channel.basic_qos(prefetch_count=self.prefetch_count)
 
-        if self.publisher_confirms:
-            LOG.debug('Enabling publisher confirms for channel %s', channel)
-            channel.confirm_delivery()
-
+    def _inform_delegates_about_channel(self, channel):
         for delegate in self.delegates:
             try:
                 delegate.on_channel_open(channel)
             except:
                 LOG.exception('Delegating on_channel_open to %s failed',
                         delegate)
+
 
     def _on_channel_closed(self, channel):
         LOG.debug('Channel %s closed', channel)
@@ -52,3 +55,73 @@ class ChannelManager(object):
                         delegate)
 
         self._channel = None
+
+    def basic_publish(success_callback=None, failure_callback=None,
+            **basic_publish_properties):
+        self._raw_publish(**basic_publish_properties)
+        if success_callback:
+            success_callback()
+
+
+    def _raw_publish(exchange_name=None, routing_key=None, message=None,
+            persistent=True, **basic_publish_properties):
+        properties = None
+        if persistent:
+            properties = pika.BasicProperties(delivery_mode=2)
+
+        return self.channel.basic_publish(exchange_name, routing_key,
+                message, properties=properties, **basic_publish_properties)
+
+
+class ConfirmingChannelManager(ChannelManager):
+    def __init__(self, max_publish_attempts=10, **kwargs):
+        self.max_publish_attempts = max_publish_attempts
+        self._unconfirmed_messages = {}
+
+        ChannelManager.__init__(self, **kwargs)
+
+    def basic_publish(attempts=0, **basic_publish_properties):
+        if attempts >= self.max_publish_attempts:
+            LOG.warn('Failed to publish message after %d attempts: %s',
+                    attempts, basic_publish_properties)
+            failure_callback = basic_publish_properties.get('failure_callback')
+            if failure_callback:
+                failure_callback()
+
+        delivery_tag = ChannelManager._raw_publish(**basic_publish_properties)
+
+        basic_publish_properties['attempts'] = attempts + 1
+        self._unconfirmed_messages[delivery_tag] = basic_publish_properties)
+
+
+    def on_confirm_ack(self, method_frame):
+        delivery_tag = method_frame.method.delivery_tag
+        basic_publish_properties = self._unconfirmed_messages.pop(delivery_tag)
+        success_callback = basic_publish_properties.get('success_callback')
+        if success_callback:
+            success_callback()
+
+    def on_confirm_nack(self, method_frame):
+        delivery_tag = method_frame.method.delivery_tag
+        basic_publish_properties = self._unconfirmed_messages.pop(delivery_tag)
+        self.basic_publish(**basic_publish_properties)
+
+
+    def _on_channel_open(self, channel):
+        self._setup_channel(channel)
+
+        if self.publisher_confirms:
+            LOG.debug('Enabling publisher confirms for channel %s', channel)
+            channel.confirm_delivery()
+
+            add_confirm_ack_callback(channel, self.on_confirm_ack)
+            add_confirm_nack_callback(channel, self.on_confirm_nack)
+
+        self._inform_delegates_about_channel(channel)
+
+
+def add_confirm_ack_callback(channel, callback):
+    channel.callbacks.add(channel.number, Basic.Ack, callback, one_shot=False)
+
+def add_confirm_nack_callback(channel, callback):
+    channel.callbacks.add(channel.number, Basic.Nack, callback, one_shot=False)
--- lib/amqp_manager/exchange_manager.py
+++ lib/amqp_manager/exchange_manager.py
@@ -1,41 +1,27 @@
 import json
 
-from pika.spec import Basic
-
 class ExchangeManager(object):
-
-    def __init__(self, exchange_name, encoder=json.dumps, exchange_type='topic',
-            durable=True, **exchange_declare_arguments):
+    def __init__(self, exchange_name, channel_manager, encoder=json.dumps,
+            exchange_type='topic', durable=True, **exchange_declare_arguments):
         self.exchange_name = exchange_name
+        self.channel_manager = channel_manager
         self.exchange_type = exchange_type
         self.durable = durable
 
         self.encoder = encoder
         self._ed_arguments = exchange_declare_arguments
 
-        self._unconfirmed_messages = {}
-
     def publish(self, routing_key, unencoded_message,
-            persistent=True, confirm_callback=None, **basic_publish_properties):
-        if confirm_callback is not None:
-            assert callable(confirm_callback)
+            **basic_publish_properties):
         encoded_message = self.encoder(unencoded_message)
 
-        self._publish_encoded_message(confirm_callback, routing_key,
-                encoded_message, persistent, basic_publish_properties)
-
-    def _publish_encoded_message(callback, routing_key, message, persistent,
-            attempts=0, **basic_publish_properties):
-        properties = None
-        if persistent:
-            properties = delivery_mode = 2
-
-        return self.channel.basic_publish(self.exchange_name, self.routing_key,
-                message, properties=properties, **basic_publish_properties)
+        self.channel_manager.basic_publish(exchange_name=self.exchange_name,
+                routing_key=routing_key, message=encoded_message,
+                **basic_publish_properties)
 
     def on_channel_open(self, channel):
-        LOG.debug('Adding publisher confirm callbacks to channel %s', channel)
-        self.channel = channel
+        LOG.debug('Declaring %s exchange %s on channel %s',
+                self.exchange_type, self.exchange_name, channel)
         channel.exchange_declare(self._on_exchange_declare_ok,
                 self.exchange_name, exchange_type=self.exchange_type,
                 durable=self.durable, arguments=self._ed_arguments)
@@ -43,52 +29,6 @@ class ExchangeManager(object):
     def on_channel_closed(self, channel):
         LOG.debug('Got on_channel_close in exchange_manager for %s',
                 self.exchange_name)
-        self.channel = None
 
     def _on_exchange_declare_ok(self, method_frame):
         LOG.debug('Exchange declare OK for exchange %s', self.exchange_name)
-
-
-
-class ConfirmingExchangeManager(ExchangeManager):
-    def _publish_encoded_message(callback, routing_key, message, persistent,
-            attempts=0, **basic_publish_properties):
-        delivery_tag = ExchangeManager._publish_encoded_message(callback,
-                routing_key, message, persistent, attempts,
-                **basic_publish_properties)
-
-        self._unconfirmed_messages[delivery_tag] = (callback, routing_key,
-                encoded_message, persistent, attempts, basic_publish_properties)
-
-    def on_confirm_ack(self, method_frame):
-        delivery_tag = method_frame.method.delivery_tag
-        (callback, routing_key, message,
-                persistent, attempts, basic_publish_properties
-                ) = self._unconfirmed_messages[delivery_tag]
-        del self._unconfirmed_messages[delivery_tag]
-        if callback:
-            callback()
-
-    def on_confirm_nack(self, method_frame):
-        delivery_tag = method_frame.method.delivery_tag
-        (callback, routing_key, message,
-                persistent, attempts, basic_publish_properties
-                ) = self._unconfirmed_messages[delivery_tag]
-        del = self._unconfirmed_messages[delivery_tag]
-        self._publish_encoded_message(callback,
-                routing_key, message, persistent,
-                attempts=attempts + 1, **basic_publish_properties)
-
-
-    def on_channel_open(self, channel):
-        ExchangeManager.on_channel_open(self, channel)
-
-        add_confirm_ack_callback(channel, self.on_confirm_ack)
-        add_confirm_nack_callback(channel, self.on_confirm_nack)
-
-
-def add_confirm_ack_callback(channel, callback):
-    channel.callbacks.add(channel.number, Basic.Ack, callback, one_shot=False)
-
-def add_confirm_nack_callback(channel, callback):
-    channel.callbacks.add(channel.number, Basic.Nack, callback, one_shot=False)
