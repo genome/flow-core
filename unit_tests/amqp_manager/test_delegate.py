08f20b6ea2f606ee57d64cc1d86257c38547a2e0
Mark Burnett <mburnett@genome.wustl.edu>
2012-12-23 22:01:44 -0600

    add delegate base class and test

---
+++ unit_tests/amqp_manager/test_delegate.py
@@ -0,0 +1,27 @@
+import unittest
+try:
+    from unittest import mock
+except:
+    import mock
+
+from amqp_manager import delegate_base
+
+class DelegateTest(unittest.TestCase):
+    def setUp(self):
+        self.ready_callback = mock.Mock()
+
+        self.children = [delegate_base.Delegate(),
+                         delegate_base.Delegate()]
+
+        self.parent = delegate_base.Delegate(self.children)
+        self.parent.add_ready_callback(self.ready_callback)
+
+    def test_notify_ready(self):
+        self.children[0].notify_ready()
+        self.assertFalse(self.ready_callback.called)
+        self.children[1].notify_ready()
+        self.ready_callback.assert_called_once_with(self.parent)
+
+
+if '__main__' == __name__:
+    unittest.main()
