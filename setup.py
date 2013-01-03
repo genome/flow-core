9c37da532c92a1f9f95b4bd01acd2bf87bc2f2ae
Mark Burnett <mburnett@genome.wustl.edu>
2013-01-03 14:44:10 -0600

    exclude tests from distribution

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ from setuptools import setup, find_packages
 setup(
         name = 'amqp_manager',
         version = '0.1',
-        packages = find_packages(),
+        packages = find_packages(exclude=[
+            'unit_tests',
+            'integration_tests',
+            'system_tests'
+        ]),
         install_requires = [
             'pika',
         ],
