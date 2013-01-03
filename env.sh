b25ee909f43ae964a21be734191bc2394e32966e
Travis Abbott <tabbott@genome.wustl.edu>
2013-01-02 21:28:13 -0600

    environment setup script

---
+++ env.sh
@@ -0,0 +1,7 @@
+export PATH=`pwd`/bin:$PATH
+export PERL5LIB=`pwd`/lib:$PERL5LIB
+export PYTHONPATH=`pwd`/lib:$PYTHONPATH
+
+export AMQP_URL=amqp://guest:guest@vmpool82
+export RUOTE_REDIS_URL=redis://vmpool83
+export INPUTS_REDIS_URL=redis://vmpool84
