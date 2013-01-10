38ebc064f05eddacdec39013de6a88edb1491e64
Mark Burnett <mburnett@genome.wustl.edu>
2013-01-10 10:10:00 -0600

    orchestrator handler now uses method_descriptors

--- flow/orchestrator/handlers.py
+++ flow/orchestrator/handlers.py
@@ -1,10 +1,10 @@
 import logging
 
-from flow.orchestrator.redisom import get_object
+from flow.orchestrator.redisom import invoke_instance_method
 
 LOG = logging.getLogger(__name__)
 
-class OrchestratorNodeHandler(object):
+class MethodDescriptorHandler(object):
     def __init__(self, redis=None, services=None, callback_name=None):
         self.redis = redis
         self.services = services
@@ -12,33 +12,17 @@ class OrchestratorNodeHandler(object):
 
 
     def message_handler(self, message):
-        # Exceptions need to be propagated back up one level
         try:
-            node = get_object(self.redis, message.return_identifier)
-        except:
-            try:
-                LOG.exception('Failed to get node from return_identifier:',
-                        message.return_identifier)
-            except AttributeError:
-                LOG.exception(
-                        'Failed to get node from return_identifier (unknown)')
-            finally:
-                raise
-
-        try:
-            callback = getattr(node, self.callback_name)
-        except AttributeError:
-            LOG.exception('Failed to get node callback')
+            method_descriptor = message.return_identifier[self.callback_name]
+        except KeyError:
+            LOG.exception('Failed to get method descriptor (%s) from message',
+                    self.callback_name)
             raise
 
         try:
-            callback(self.services, **message.to_dict())
-        except NodeAlreadyCompletedError:
-            LOG.exception('%s got node already completed error',
-                    self.__class__.__name__)
-        except NodeAlreadyExecutedError:
-            LOG.exception('%s got node already executed error',
-                    self.__class__.__name__)
-        except RuntimeError:
-            LOG.exception('Failed to execute node callback')
+            invoke_instance_method(method_descriptor)
+
+        except:
+            LOG.error('Handler (%s) failed to execute method descriptor: %s',
+                    self, method_descriptor)
             raise
