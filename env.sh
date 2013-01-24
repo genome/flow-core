d7cae40c961408fd95046f2a2e3869468f98cd4c
Travis Abbott <tabbott@genome.wustl.edu>
2013-01-24 12:46:05 -0600

    change redis and rabbit servers in env.sh

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@ export FLOW_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 export PATH=$FLOW_HOME/bin:$PATH
 export AMQP_URL=amqp://guest:guest@vmpool82:5672/workflow
-export FLOW_REDIS_URL=blade14-2-5
+export FLOW_REDIS_URL=vmpool83
 
 echo ""
 echo "Setting up flow base environment in $FLOW_HOME"
