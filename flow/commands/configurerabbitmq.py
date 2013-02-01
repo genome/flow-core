6e052b187aeac9b9108e0c3ca11d15e96d3cbebb
Anthony Brummett <abrummet@genome.wustl.edu>
2013-02-01 10:58:45 -0600

    Refactored the old bin/configure-rabbitmq to here

--- flow/commands/configurerabbitmq.py
+++ flow/commands/configurerabbitmq.py
@@ -1,15 +1,18 @@
-import argparse
 from flow.commands.base import CommandBase
 import logging
-import time
-import uuid
+import sys
+import json
+import itertools
 
 LOG = logging.getLogger()
 
 
 class ConfigureRabbitMQCommand(CommandBase):
     def __init__(self, bindings={}, vhost=''):
-        print "in __init__ vhost %s bindings %s" % (vhost, bindings)
+
+        self.bindings_config = bindings
+        self.vhost = vhost
+        self.rabbit_configuration = None
 
     @staticmethod
     def annotate_parser(parser):
@@ -17,6 +20,135 @@ class ConfigureRabbitMQCommand(CommandBase):
                             default=None, help='Filename to write to. Defaults to STDOUT')
 
     def __call__(self, parsed_arguments):
-        print "in __call__ self %s parsed_args %s" % (self, parsed_arguments)
+
+        if self.rabbit_configuration is None:
+            self.rabbit_configuration = self.empty_rabbit_configuration()
+
+            self.parse_exchanges_queues_bindings()
+
+            self.make_exchange_defs()
+            self.make_queue_defs()
+            self.make_binding_defs()
+
+        self.dump_config_to_file(parsed_arguments.output_filename)
 
         return 0
+
+    def parse_exchanges_queues_bindings(self):
+        exchanges = self.exchanges = set()
+        queues = self.queues = set()
+        bindings = self.bindings = set()
+
+        for exchange_name, queue_bindings in self.bindings_config.iteritems():
+            exchanges.add(exchange_name)
+            for queue_name, topics in queue_bindings.iteritems():
+                queues.add(queue_name)
+                for topic in topics:
+                    bindings.add( (exchange_name, topic, queue_name) )
+
+    def make_exchange_defs(self):
+
+        def new_exchange(name):
+            if name == 'alt':
+                args = {}
+            else:
+                args = { 'altername-exchange': 'alt'}
+
+            return {'name': name,
+                    'vhost': self.vhost,
+                    'durable': True,
+                    'auto_delete': False,
+                    'internal': False,
+                    'type': 'topic',
+                    'arguments': args,
+                   }
+
+        result = []
+        for exch_name in itertools.chain(['alt'], self.exchanges, ['dead']):
+            result.append( new_exchange(exch_name) )
+        self.rabbit_configuration['exchanges'] = result
+
+    def make_queue_defs(self):
+
+        def new_queue(name):
+            if name == 'missing_routing_key' or name.startswith('dead'):
+                args = {}
+            else:
+                args = { 'x-dead-letter-exchange': 'dead'}
+
+            return {'name': name,
+                    'vhost': self.vhost,
+                    'durable': True,
+                    'auto_delete': False,
+                    'arguments': args,
+                    }
+
+
+        result = []
+        for queue_name in itertools.chain(['missing_routing_key'], self.queues):
+            result.append( new_queue(queue_name) )
+
+            if queue_name != 'missing_routing_key':
+                result.append( new_queue('dead_' + queue_name) )
+        self.rabbit_configuration['queues'] = result
+
+
+    def make_binding_defs(self):
+
+        def new_binding(source, routing_key, destination):
+            return {'vhost': self.vhost,
+                    'source': source,
+                    'routing_key': routing_key,
+                    'destination': destination,
+                    'destination_type': 'queue',
+                    'arguments': {},
+                    }
+
+        result = [ new_binding('alt', '#', 'missing_routing_key') ]
+        for name, topic, queue_name in self.bindings:
+            result.append( new_binding( name, topic, queue_name ) )
+            result.append( new_binding( 'dead', topic, 'dead_' + queue_name) )
+        self.rabbit_configuration['bindings'] = result
+
+
+    def dump_config_to_file(self, output_filename):
+        if output_filename:
+            of = open(output_filename, 'w')
+        else:
+            of = sys.stdout
+
+        json.dump(self.rabbit_configuration, of)
+
+
+    def empty_rabbit_configuration(self):
+        return {"rabbit_version":"3.0.1",
+            "parameters":[],
+            "policies":[],
+            "users":[
+                {"name":"guest",
+                "password_hash":"GvVZRv7FY1mtJZfvN42rcdkLQ/w=",
+                "tags":"administrator"}
+            ],
+            "vhosts":[{"name":"/"}, {"name":"workflow"}, {"name":"testing"}],
+
+            "permissions":[
+                {"user":"guest",
+                "vhost":"/",
+                "configure":".*",
+                "write":".*",
+                "read":".*"},
+
+                {"user":"guest",
+                "vhost":"workflow",
+                "configure":".*",
+                "write":".*",
+                "read":".*"},
+
+                {"user":"guest",
+                "vhost":"testing",
+                "configure":".*",
+                "write":".*",
+                "read":".*"}
+            ],
+        }
+
