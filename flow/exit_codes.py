acbb04bc03f3bebdbc6209421490206542069cf3
Mark Burnett <mburnett@genome.wustl.edu>
2013-04-08 13:30:33 -0500

    add exit_codes module

---
+++ flow/exit_codes.py
@@ -0,0 +1,13 @@
+import os
+
+# Success and Failure are normal modes of operation
+EXECUTE_SUCCESS = os.EX_OK
+EXECUTE_FAILURE = 1
+
+# Error means there is probably a bug and we should reject doing this work.
+EXECUTE_ERROR = os.EX_SOFTWARE
+
+# System failure means there is probably system instability and we
+#   should retry.  Retrying usually means re-delivering a messages and/or
+#   restarting a service.
+EXECUTE_SYSTEM_FAILURE = os.EX_TEMPFAIL
