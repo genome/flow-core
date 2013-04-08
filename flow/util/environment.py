014a9a572cab8457e649bfc7707df8feeb7ca910
Mark Burnett <mburnett@genome.wustl.edu>
2013-04-08 10:42:08 -0500

    refactor set_environment function out of context manager

--- flow/util/environment.py
+++ flow/util/environment.py
@@ -6,17 +6,21 @@ LOG = logging.getLogger(__name__)
 
 @contextmanager
 def environment(environment_dicts):
+    saved_environment = dict(os.environ.data)
+    set_environment(*environment_dicts)
+
+    try:
+        yield
+    finally:
+        os.environ.clear()
+        os.environ.update(saved_environment)
+
+def set_environment(*environment_dicts):
     temporary_environment = dict()
     for env in environment_dicts:
         temporary_environment.update(env)
 
-    for k, v in temporary_environment.iteritems():
-        if v is None:
-            temporary_environment[k] = ''
-
-    saved_environment = dict(os.environ.data)
     os.environ.clear()
-
     for key, value in temporary_environment.iteritems():
         try:
             os.environ[key] = value
@@ -24,9 +28,3 @@ def environment(environment_dicts):
             LOG.warn('Failed to update environment variable %s=%s... skipping',
                     key, value)
             pass
-
-    try:
-        yield
-    finally:
-        os.environ.clear()
-        os.environ.update(saved_environment)
