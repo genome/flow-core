d20cb0f27a09376f17f17a48fe1a287adfd8bbfe
David Morton <davidlmorton@gmail.com>
2013-04-17 13:23:05 -0500

    TokenSenders now use an orchestrator to set_tokens

--- flow/commands/token_sender.py
+++ flow/commands/token_sender.py
@@ -2,6 +2,7 @@ from flow.commands.base import CommandBase
 from flow.configuration.inject.broker import BlockingBrokerConfiguration
 from flow.configuration.inject.redis_conf import RedisConfiguration
 from flow.configuration.settings.injector import setting
+from flow.service_locator import ServiceLocator
 from flow.petri.netbase import Token, SetTokenMessage
 from injector import inject
 
@@ -12,7 +13,8 @@ import logging
 LOG = logging.getLogger(__name__)
 
 
-@inject(broker=flow.interfaces.IBroker, storage=flow.interfaces.IStorage,
+@inject(service_locator=ServiceLocator,
+        storage=flow.interfaces.IStorage,
         exchange=setting('send_token.exchange'),
         routing_key=setting('send_token.routing_key'))
 class TokenSenderCommand(CommandBase):
@@ -22,13 +24,14 @@ class TokenSenderCommand(CommandBase):
     ]
 
     def send_token(self, net_key=None, place_idx=None, data=None):
-        self.broker.connect()
+        orchestrator = self.service_locator['orchestrator']
+        orchestrator.broker.connect()
+
         token = Token.create(self.storage, data=data, data_type="output")
 
         LOG.info("Sending command response token %s to net %s, place %r",
                 token.key, net_key, place_idx)
-        message = SetTokenMessage(net_key=net_key, place_idx=place_idx,
+        orchestrator.set_token(net_key=net_key, place_idx=int(place_idx),
                 token_key=token.key)
-        self.broker.publish(exchange_name=self.exchange, routing_key=self.routing_key,
-                message=message)
-        self.broker.disconnect()
+
+        orchestrator.broker.disconnect()
