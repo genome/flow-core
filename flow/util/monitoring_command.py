ad368fa7c776040664ba4df2d14e9585ac22f41a
David Morton <davidlmorton@gmail.com>
2013-05-08 11:28:23 -0500

    Improve monitoring_command and reading_command

--- flow/util/monitoring_command.py
+++ flow/util/monitoring_command.py
@@ -1,5 +1,5 @@
 from flow.commands.base import CommandBase
-from twisted.internet import defer
+from twisted.internet import defer, reactor
 from flow.util.process_monitor import ProcessMonitor
 from flow.util.logannotator import LogAnnotator
 import os
@@ -8,27 +8,41 @@ class MonitoringCommand(CommandBase):
     injector_modules = []
     @staticmethod
     def annotate_parser(parser):
-        parser.add_argument('--num-processes', '-n', type=int)
-        parser.add_argument('--port', '-p', type=int)
         parser.add_argument('--file', '-f', type=str)
-        parser.add_argument('--time-per-read', '-t', type=float)
-        parser.add_argument('--read-size', '-s', type=int)
-        parser.add_argument('--repeat', '-r', default=False, action='store_true')
+        parser.add_argument('--port', '-p', type=int)
 
     def _execute(self, parsed_arguments):
-
         process_monitor = ProcessMonitor(os.getpid())
         process_monitor.start(port=parsed_arguments.port)
 
-        deferreds = []
-        for i in range(parsed_arguments.num_processes):
-            cmdline = ['flow', 'reading-command',
-                    '-f', parsed_arguments.file,
-                    '-s', str(parsed_arguments.read_size),
-                    '-t', str(parsed_arguments.time_per_read)]
-            if parsed_arguments.repeat:
-                cmdline.append('-r')
-            logannotator = LogAnnotator(cmdline)
-            deferreds.append(logannotator.start())
+        cmdline = ['flow', 'reading-command',
+                '-f', parsed_arguments.file,
+                '-s', str(256),
+                '-r',
+                '-t', str(0.5)]
+        logannotator = LogAnnotator(cmdline)
+        long_deferred = logannotator.start()
 
-        return defer.DeferredList(deferreds)
+        cmdline = ['flow', 'reading-command',
+                '-f', parsed_arguments.file,
+                '-s', str(128),
+                '-r',
+                '-j',
+                '-t', str(0.75)]
+        logannotator = LogAnnotator(cmdline)
+        logannotator.start()
+
+        periodic_deferred = self._start_reading(parsed_arguments.file)
+
+        return long_deferred
+
+    def _start_reading(self, filename):
+        cmdline = ['flow', 'reading-command',
+                '-f', filename,
+                '-s', str(32768),
+                '-t', str(2.0)]
+        logannotator = LogAnnotator(cmdline)
+        this_deferred = logannotator.start()
+
+        this_deferred.addCallback(
+                lambda *args: reactor.callLater(5.0, self._start_reading, filename))
--- flow/util/reading_command.py
+++ flow/util/reading_command.py
@@ -1,6 +1,8 @@
 from flow.commands.base import CommandBase
 from twisted.internet import defer, reactor
 import time
+import os
+import random
 
 class ReadingCommand(CommandBase):
     injector_modules = []
@@ -10,22 +12,32 @@ class ReadingCommand(CommandBase):
         parser.add_argument('--time-per-read', '-t', type=float)
         parser.add_argument('--read-size', '-s', type=int)
         parser.add_argument('--repeat', '-r', default=False, action='store_true')
+        parser.add_argument('--jump-around', '-j', default=False, action='store_true')
 
     @defer.inlineCallbacks
     def _execute(self, parsed_arguments):
+        filename = parsed_arguments.file
+        file_size = os.stat(filename).st_size
         while True:
-            with open(parsed_arguments.file, 'r') as infile:
+            with open(filename, 'r', 0) as infile:
                 while True:
+                    jump_to = -1
+                    if parsed_arguments.jump_around:
+                        jump_to = random.randint(0, file_size)
                     data = yield self._read_data(infile,
                             parsed_arguments.read_size,
-                            parsed_arguments.time_per_read)
+                            parsed_arguments.time_per_read,
+                            jump_to=jump_to)
                     if data == '':
                         break
             if not parsed_arguments.repeat:
                 break
 
-    def _read_data(self, fh, size, delay):
-        deferred = defer.Deferred()
+    def _read_data(self, fh, size, delay, jump_to=-1):
+        if jump_to >= 0:
+            fh.seek(jump_to)
         data = fh.read(size)
+
+        deferred = defer.Deferred()
         reactor.callLater(delay, deferred.callback, data)
         return deferred
