abe608cc1d03c1be0ccb5d235e3a940263c695a8
Mark Burnett <mburnett@genome.wustl.edu>
2013-06-05 09:15:58 -0500

    fix clear data action return value

--- flow/petri_net/actions/clear_data.py
+++ flow/petri_net/actions/clear_data.py
@@ -1,11 +1,12 @@
 from flow.petri_net.actions.base import BarrierActionBase, BasicActionBase
+from twisted.internet import defer
 
 
 class BasicClearDataAction(BasicActionBase):
     def execute(self, net, color_descriptor, active_tokens, service_interfaces):
         token = net.create_token(color=color_descriptor.color,
                 color_group_idx=color_descriptor.color_group.idx)
-        return [token]
+        return [token], defer.succeed(None)
 
 class BarrierClearDataAction(BarrierActionBase):
     def execute(self, net, color_descriptor, active_tokens, service_interfaces):
@@ -15,4 +16,4 @@ class BarrierClearDataAction(BarrierActionBase):
             tokens.append(net.create_token(color=color,
                 color_group_idx=cg.idx))
 
-        return tokens
+        return tokens, defer.succeed(None)
