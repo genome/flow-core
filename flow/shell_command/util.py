112dada835b7e31dbf9e241ade8ec8320f17d238
Mark Burnett <mburnett@genome.wustl.edu>
2013-06-05 12:18:04 -0500

    remove unused seteuid context manager

--- flow/shell_command/util.py
+++ flow/shell_command/util.py
@@ -1,30 +1,8 @@
-from contextlib import contextmanager
 import logging
 import os
 
+
 LOG = logging.getLogger(__name__)
-
-@contextmanager
-def seteuid(user_id):
-    if user_id is not None:
-        saved_user_id = os.geteuid()
-        if user_id != saved_user_id:
-            LOG.debug('Setting uid to 0, then to %d', user_id)
-            os.seteuid(0)
-            os.seteuid(user_id)
-        else:
-            LOG.debug('Uid is already %d, not changing', user_id)
-
-    try:
-        yield
-    finally:
-        if user_id is not None:
-            if saved_user_id != os.geteuid():
-                os.seteuid(0)
-                os.seteuid(saved_user_id)
-                LOG.debug('uid reset to %d', saved_user_id)
-            else:
-                LOG.debug('Uid is already %d, not changing', user_id)
 
 
 def join_path_if_rel(*path_components):
