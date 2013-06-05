1b5199fa406777dbb5b298656c25012261f3a375
Mark Burnett <mburnett@genome.wustl.edu>
2013-06-05 08:42:42 -0500

    remove unused import from fork_service command

--- flow/shell_command/commands/fork_service.py
+++ flow/shell_command/commands/fork_service.py
@@ -1,12 +1,12 @@
-from flow.shell_command.handler import ForkShellCommandMessageHandler
 from flow.commands.service import ServiceCommand
 from flow.configuration.inject.broker import BrokerConfiguration
 from flow.configuration.inject.fork_executor import ForkExecutorConfiguration
 from flow.configuration.inject.redis_conf import RedisConfiguration
 from flow.configuration.inject.service_locator import ServiceLocatorConfiguration
-from injector import inject
+from flow.shell_command.handler import ForkShellCommandMessageHandler
 
 import logging
+
 
 LOG = logging.getLogger(__name__)
 
