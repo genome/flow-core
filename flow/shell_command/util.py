73a3fd0fcb667d6f8ecc88bcddd1dd045d973783
Mark Burnett <mburnett@genome.wustl.edu>
2013-06-11 17:20:24 -0500

    remove shell_command.util.join_path_if_rel

--- flow/shell_command/util.py
+++ flow/shell_command/util.py
@@ -9,30 +9,6 @@ import socket
 LOG = logging.getLogger(__name__)
 
 
-def join_path_if_rel(*path_components):
-    try:
-        final_path_component = path_components[-1]
-    except IndexError:
-        raise RuntimeError('Not enough arguments given.')
-
-    try:
-        final_pc_is_abs = os.path.isabs(final_path_component)
-    except AttributeError:
-        raise RuntimeError('Could not determine whether final path component '
-                           'was absolute path (probably not a string): %s' %
-                ' + '.join(['(%r)' % pc for pc in path_components]))
-
-    if not final_pc_is_abs:
-        try:
-            return os.path.join(*path_components)
-        except AttributeError:
-            raise RuntimeError('Failed to join path components '
-                               '(probably non-string components): %s' %
-                    ' + '.join(['(%r)' % pc for pc in path_components]))
-
-    return final_path_component
-
-
 def wait_for_pid(pid):
     _, exit_status = os.waitpid(pid, 0)
     signal_number = 255 & exit_status
--- unit_tests/shell_command/test_util.py
+++ unit_tests/shell_command/test_util.py
@@ -2,31 +2,7 @@ from flow.shell_command import util
 from uuid import uuid4
 
 import mock
-import os
 import unittest
-
-
-class JoinPathIfRelTest(unittest.TestCase):
-    def test_is_relative(self):
-        parts = ['/root', 'middle', 'relative/guy.txt']
-        self.assertEqual(util.join_path_if_rel(*parts),
-                '/root/middle/relative/guy.txt')
-
-    def test_is_absolute(self):
-        parts = ['/root', 'middle', '/absolute/guy.txt']
-        self.assertEqual(util.join_path_if_rel(*parts),
-                '/absolute/guy.txt')
-
-    def test_error_in_final_path_component(self):
-        parts = ['/root', 'middle', None]
-        self.assertRaises(RuntimeError, util.join_path_if_rel, *parts)
-
-    def test_error_in_other_path_components(self):
-        parts = ['/root', None, 'relative/guy.txt']
-        self.assertRaises(RuntimeError, util.join_path_if_rel, *parts)
-
-    def test_not_enough_arguments(self):
-        self.assertRaises(RuntimeError, util.join_path_if_rel)
 
 
 if '__main__' == __name__:
