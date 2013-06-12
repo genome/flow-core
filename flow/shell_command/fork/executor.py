17d9d76613279691e764262a50681cbc3ce81913
Mark Burnett <mburnett@genome.wustl.edu>
2013-06-12 09:26:34 -0500

    separate file opening in fork executor

--- flow/shell_command/fork/executor.py
+++ flow/shell_command/fork/executor.py
@@ -2,6 +2,7 @@ from flow import exit_codes
 from flow.shell_command.executor_base import ExecutorBase, send_message
 from twisted.internet import defer
 
+import contextlib
 import logging
 import socket
 import subprocess
@@ -37,24 +38,12 @@ class ForkExecutor(ExecutorBase):
 
     def execute_command_line(self, job_id_callback, command_line,
             executor_data, resources):
-        stderr = executor_data.get('stderr')
-        stdin = executor_data.get('stdin')
-        stdout = executor_data.get('stdout')
 
-        stderr_fh = None
-        stdin_fh = None
-        stdout_fh = None
-        try:
-            if stderr:
-                stderr_fh = open(stderr, 'a')
-            if stdin:
-                stdin_fh = open(stdin, 'r')
-            if stdout:
-                stdout_fh = open(stdout, 'a')
-
+        with open_files(executor_data) as (stderr, stdin, stdout):
             LOG.debug('executing command %s', " ".join(command_line))
+
             p = subprocess.Popen(command_line, close_fds=True,
-                    stderr=stderr_fh, stdin=stdin_fh, stdout=stdout_fh)
+                    stderr=stderr, stdin=stdin, stdout=stdout)
             job_id_callback(p.pid)
 
             returncode = p.wait()
@@ -64,15 +53,32 @@ class ForkExecutor(ExecutorBase):
             else:
                 exit_code = returncode
 
-        except OSError:
-            LOG.exception('Executor got OSError')
-            raise
-        finally:
-            if stderr_fh:
-                stderr_fh.close()
-            if stdin_fh:
-                stdin_fh.close()
-            if stdout_fh:
-                stdout_fh.close()
-
         return exit_code
+
+
+@contextlib.contextmanager
+def open_files(executor_data):
+    stderr = executor_data.get('stderr')
+    stdin = executor_data.get('stdin')
+    stdout = executor_data.get('stdout')
+
+    stderr_fh = None
+    stdin_fh = None
+    stdout_fh = None
+    try:
+        if stderr:
+            stderr_fh = open(stderr, 'a')
+        if stdin:
+            stdin_fh = open(stdin, 'r')
+        if stdout:
+            stdout_fh = open(stdout, 'a')
+
+        yield stderr_fh, stdin_fh, stdout_fh
+
+    finally:
+        if stderr_fh:
+            stderr_fh.close()
+        if stdin_fh:
+            stdin_fh.close()
+        if stdout_fh:
+            stdout_fh.close()
