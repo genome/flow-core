f4cdf26129a69146ce1f1e0e3cd9c40f6eb77b1d
Mark Burnett <mburnett@genome.wustl.edu>
2013-07-10 15:24:14 -0500

    make ServiceLocator lazy-load services

--- flow/service_locator.py
+++ flow/service_locator.py
@@ -7,12 +7,20 @@ import pkg_resources
 class ServiceLocator(flow.interfaces.IServiceLocator):
     def __init__(self):
         self._services = {}
-        for ep in pkg_resources.iter_entry_points('flow.services'):
+
+    def _get_missing_service(self, name):
+        for ep in pkg_resources.iter_entry_points('flow.services', name):
             cls = ep.load()
-            self._services[ep.name] = self.i.get(cls)
+            service = self.i.get(cls)
+            self._services[ep.name] = service
+            break
+        return service
 
     def __getitem__(self, name):
-        return self._services[name]
+        try:
+            return self._services[name]
+        except KeyError:
+            return self._get_missing_service(name)
 
     def get(self, name, default=None):
         try:
