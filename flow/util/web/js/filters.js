2fba25e384a27867498407f5532270a636c6eceb
Joshua McMichael <jmcmicha@genome.wustl.edu>
2013-09-20 14:43:16 -0500

    added filter that converts bytes into 'human-readable' strings w/ KB, MB, etc

--- flow/util/web/js/filters.js
+++ flow/util/web/js/filters.js
@@ -10,4 +10,22 @@ angular.module('processMonitor.filters', [])
                 return input;
             }
         }
+    })
+    .filter('humanReadableBytes', function() {
+        return function (bytes) {
+            if( isNaN( bytes ) ){ return bytes; }
+            var units = [ ' bytes', ' KB', ' MB', ' GB', ' TB', ' PB', ' EB', ' ZB', ' YB' ];
+            var amountOf2s = Math.floor( Math.log( +bytes )/Math.log(2) );
+            if( amountOf2s < 1 ){
+                amountOf2s = 0;
+            }
+            var i = Math.floor( amountOf2s / 10 );
+            bytes = +bytes / Math.pow( 2, 10*i );
+
+            // Rounds to 3 decimals places.
+            if( bytes.toString().length > bytes.toFixed(3).toString().length ){
+                bytes = bytes.toFixed(3);
+            }
+            return bytes + units[i];
+        }
     });
