08932273820e397a95f811fba9c0f5e4d99ca2de
Mark Burnett <mburnett@genome.wustl.edu>
2013-09-25 13:12:06 -0500

    make command_class optional for initialize_injector

--- flow/configuration/inject/initialize.py
+++ flow/configuration/inject/initialize.py
@@ -6,9 +6,13 @@ import injector
 INJECTOR = injector.Injector()
 
 
-def initialize_injector(settings, command_class):
-    add_modules(INJECTOR, InjectedSettings(settings),
-            *command_class.injector_modules)
+def initialize_injector(settings, command_class=None):
+    if command_class:
+        args = command_class.injector_modules
+    else:
+        args = []
+
+    add_modules(INJECTOR, InjectedSettings(settings), *args)
 
     return INJECTOR
 
