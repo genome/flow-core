468edc591de8df00fa75d684fb2edaf3329b2c5d
Mark Burnett <mburnett@genome.wustl.edu>
2013-09-25 14:42:55 -0500

    remove unused import

--- flow/shell_command/lsf/handler.py
+++ flow/shell_command/lsf/handler.py
@@ -1,7 +1,6 @@
 from flow.configuration.settings.injector import setting
 from flow.interfaces import IServiceLocator
 from flow.shell_command.handler_base import ShellCommandSubmitMessageHandler
-from flow.shell_command.lsf import executor
 from injector import inject
 
 
